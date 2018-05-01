40c94f9c12225854db6862b258d12a5ccde48b3a
rigojr <rigojunnior@gmail.com>
2018-05-01 11:35:01 -0400

    cambios para probar el breadcrumb

--- page.php
+++ page.php
@@ -1,13 +1,5 @@
 <?php get_header(); ?>
 
-<?php get_template_part( 'breadcrumb-normal' ); ?>
-
-<div class="wrap-page-content">
-	<?php if ( have_posts() ) : the_post(); ?>
-  		<?php the_content(); ?>
-	<?php endif; ?>
-</div>
-
-<?php get_template_part( 'call-contact' ); ?>
+<?php get_template_part( 'breadcrumb' ); ?>
 
 <?php get_footer(); ?>
