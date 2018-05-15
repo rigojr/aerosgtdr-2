b93b4331370cbb13d63c542269f2de199a4479a6
Brian <brianjesusmoreno@gmail.com>
2018-05-15 19:47:58 -0400

    arreglo errorcito

--- assets/js/scroll-easy.js
+++ assets/js/scroll-easy.js
@@ -1,6 +1,6 @@
 $(function(){
 
-     $('a[href*=#]').click(function() {
+     $('a[href*=\\#]').click(function() {
 
      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
          && location.hostname == this.hostname) {
